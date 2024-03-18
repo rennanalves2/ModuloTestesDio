f7ed58c17812aa7f1a10d2a60e435ccb7587bb41
Rennan Alves <rennan.alves@blip.ai>
2024-03-18 16:51:04 -0300

    Test implementation

--- CalculadoraTestes/CalculadoraTests.cs
+++ CalculadoraTestes/CalculadoraTests.cs
@@ -1,16 +1,28 @@
+using Calculadora.Services;
+using NuGet.Frameworks;
+
 namespace CalculadoraTestes;
 
 public class CalculadoraTestes
 {
+    private CalculadoraImp _calc;
+
+    public CalculadoraTestes()
+    {
+        _calc = new CalculadoraImp();
+    }
+
     [Fact]
     public void DeveSomar5Com10ERetornar15()
     {
         // Arrange
-
+        int num1 = 5;
+        int num2 = 10;
 
         // Act
-
+        var result = _calc.Somar(num1, num2);
 
         // Assert
+        Assert.Equal(15, result);
     }
 }
