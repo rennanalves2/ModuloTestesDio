561ceebae393ee5e3d6ff2be92b5667dd5730583
Rennan Alves <rennan.alves@blip.ai>
2024-03-18 16:42:45 -0300

    Add project files.

---
+++ Calculadora/Services/CalculadoraImp.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Calculadora.Services
+{
+    public class CalculadoraImp
+    {
+        public int Somar(int x, int y) {
+            return x + y;
+        }
+    }
+}
---
+++ CalculadoraTestes/CalculadoraTests.cs
@@ -0,0 +1,16 @@
+namespace CalculadoraTestes;
+
+public class CalculadoraTestes
+{
+    [Fact]
+    public void DeveSomar5Com10ERetornar15()
+    {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+    }
+}
